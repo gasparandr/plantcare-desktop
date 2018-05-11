efa6ca8c5958f27709335f43cfff971a67bd98f1
Andrei Gaspar <andrei@ildiesign.com>
2018-05-11 21:45:29 +0300

    - Component interface adjustments

--- src/components/IComponent.ts
+++ src/components/IComponent.ts
@@ -2,7 +2,10 @@
 
 namespace Components {
     export interface IComponent {
+        NAME: string;
         eventHandler(notification: string, data: any): void;
+        registerEventInterests(): void;
+        registerEventListeners(): void;
     }
 
 }
