c5b549313b42273915106aad286146fc355ec765
Andrei Gaspar <andrei@ildiesign.com>
2018-05-11 21:47:00 +0300

    - Notification consants for login

---
+++ src/constants/Notifications.ts
@@ -0,0 +1,9 @@
+
+
+
+namespace Constants {
+    export class Notifications {
+        public static LOGIN_SUCCESS: string = "LOGIN_SUCCESS";
+        public static LOGIN_FAILURE: string = "LOGIN_FAILURE";
+    }
+}
