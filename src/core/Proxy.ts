dce59f8d7e67b45a01372f78a54ce42ee38381d4
Andrei Gaspar <andrei@ildiesign.com>
2018-05-11 21:36:55 +0300

    - Proxy API mockup

---
+++ src/core/Proxy.ts
@@ -0,0 +1,54 @@
+
+
+namespace Core {
+
+    export class Proxy {
+        public NAME: string = "Proxy";
+        private address: string;
+        private userId: string;
+
+        constructor() {
+            this.address = "";
+        }
+        
+
+        public login(username: string, password: string): void {
+
+        }
+
+        public getPlants(plantGroupId: string): void {
+
+        }
+
+        public getPlantGroups(): void {
+
+        }
+
+        public invite(): void {
+
+        }
+
+        public getInvites(): void {
+
+        }
+
+        public waterPlant(plantId: string): void {
+
+        }
+
+        public waterPlantGroup(plantGroupId: string): void {
+
+        }
+
+        public pingForInvites(): void {
+
+        }
+
+        public cancelPing(): void {
+
+        }
+
+    }
+
+
+}
