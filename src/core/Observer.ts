42b037c752158ad92abecc990b75dd194d781e7d
Andrei Gaspar <andrei@ildiesign.com>
2018-05-11 21:44:57 +0300

    - Implemented observer

---
+++ src/core/Observer.ts
@@ -0,0 +1,44 @@
+
+
+namespace Core {
+    export class Observer {
+        public static NAME: string = "Observer";
+        private notificationInterests: any;
+
+
+        constructor() {
+
+            this.notificationInterests = {};
+        }
+
+        public registerEventInterest(entity: any, notification:string): void {
+
+            if ( ! this.notificationInterests[ notification ] ) {
+                this.notificationInterests[ notification ] = [];
+                this.notificationInterests[ notification ].push(entity);
+            } else {
+                for (let i = 0; i < this.notificationInterests[notification].length; i++) {
+                    if (  this.notificationInterests[notification][i].NAME = entity.NAME ) {
+                        console.warn(entity.NAME + " is already registered to the notification " + notification);
+                        return;
+                    }
+                }
+
+                this.notificationInterests[notification].push(entity);
+            }
+
+        }
+
+        public sendNotification(notification: string, data: any) {
+            const interestedEntities = this.notificationInterests[ notification ];
+
+            for (let i = 0; i < interestedEntities.length; i++) {
+                interestedEntities[i].eventHandler( notification, data );
+            }
+
+            if ( ! interestedEntities.length ) console.warn("There is no entity interested in the notification: " + notification);
+        }
+
+
+    }
+}
