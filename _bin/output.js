53ab0d5fbab380bac7e954cb0b8401961d425c6a
Andrei Gaspar <andrei@ildiesign.com>
2018-05-12 23:22:51 +0300

    - Generate mock data

--- _bin/output.js
+++ _bin/output.js
@@ -452,10 +452,11 @@ var viewManager = new ViewManager();
 var Components;
 (function (Components) {
     var PlantGroupCard = (function () {
-        function PlantGroupCard() {
+        function PlantGroupCard(config) {
             this.NAME = "PlantGroupCard";
             console.info(this.NAME + " has been initiated");
             this.registerEventInterests();
+            this.generateMockData();
             this.container = document.getElementById("container");
             this.registerEventListeners();
         }
@@ -464,6 +465,24 @@ var Components;
         PlantGroupCard.prototype.registerEventListeners = function () {
         };
         PlantGroupCard.prototype.injectHTML = function () {
+        };
+        PlantGroupCard.prototype.generateMockData = function () {
+            this.speciesCount = Math.floor(Math.random() * 9) + 4;
+            this.plantCount = Math.floor(Math.random() * 22) + 6;
+            var lastWateredDay = Math.floor(Math.random() * 18) + 1;
+            this.lastWateredDate = lastWateredDay + " May 2018";
+            var randomFreq = Math.floor(Math.random() * 12) + 1;
+            this.nextWateringDate = (randomFreq + lastWateredDay) + " May 2018";
+            this.percent = Math.floor(Math.random() * 100) + 1;
+            if (this.percent <= 33) {
+                this.statusClassName = "";
+            }
+            else if (this.percent <= 66) {
+                this.statusClassName = "";
+            }
+            else {
+                this.statusClassName = "";
+            }
         };
         PlantGroupCard.prototype.eventHandler = function (notification, data) {
             switch (notification) {
--- src/components/PlantGroupCard.ts
+++ src/components/PlantGroupCard.ts
@@ -5,10 +5,20 @@ namespace Components {
     export class PlantGroupCard implements IComponent {
         public NAME: string = "PlantGroupCard";
         private container: HTMLElement;
+        private speciesCount: number;
+        private plantCount: number;
+        private name: string;
+        private lastWateredDate: string;
+        private nextWateringDate: string;
+        private percent: number;
+        private statusClassName: string;
 
-        constructor() {
+
+        constructor(config: any) {
             console.info( this.NAME + " has been initiated");
             this.registerEventInterests();
+
+            this.generateMockData();
 
             this.container = document.getElementById("container");
 
@@ -26,6 +36,24 @@ namespace Components {
         }
 
         public injectHTML(): void {
+        }
+
+        private generateMockData(): void {
+            this.speciesCount = Math.floor(Math.random() * 9) + 4;
+            this.plantCount = Math.floor(Math.random() * 22) + 6;
+            const lastWateredDay = Math.floor(Math.random() * 18) + 1;
+            this.lastWateredDate = lastWateredDay + " May 2018";
+            const randomFreq = Math.floor(Math.random() * 12) + 1;
+            this.nextWateringDate = (randomFreq + lastWateredDay) + " May 2018";
+            this.percent = Math.floor(Math.random() * 100) + 1;
+
+            if ( this.percent <= 33) {
+                this.statusClassName = "";
+            } else if (this.percent <= 66) {
+                this.statusClassName = "";
+            } else {
+                this.statusClassName = "";
+            }
         }
 
         public eventHandler(notification: string, data: any): void {
